cc2bf46ed68bf16bbe0e51bde77c232747708a25
seagalputra <seagalputra@gmail.com>
2019-06-10 15:33:41 +0700

    Fix content and modify README.md

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 [![Codemagic build status](https://api.codemagic.io/apps/5cfd1a30082482000c4d84ed/5cfd1a30082482000c4d84ec/status_badge.svg)](https://codemagic.io/apps/5cfd1a30082482000c4d84ed/5cfd1a30082482000c4d84ec/latest_build)
 
 Let's discover outer space! :stars:
+
+---
+This apps is for Online Study Group Assignment from Eudeka
+'''
+OSG04054 - Dwiferdio Seagal Putra
+'''
--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,12 @@ import 'package:flutter/widgets.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  
   // definisi list dari data
   List<Card> data = [
     Card(
       semanticContainer: true,
       clipBehavior: Clip.antiAliasWithSaveLayer,
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10.0)
-      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
       margin: EdgeInsets.all(10),
       elevation: 5,
       child: Column(
@@ -31,12 +28,12 @@ class MyApp extends StatelessWidget {
                     onPressed: () {
                       print('I Like it!');
                     },
-                  )
-              ),
+                  )),
               Container(
                 padding: EdgeInsets.fromLTRB(16, 1, 16, 16),
-                child: Text('Merkurius adalah planet terkecil di dalam tata surya dan juga yang terdekat dengan Matahari dengan kala revolusi 88 hari dan kala rotasi 59 hari. ' 
-                'Kecerahan planet ini berkisar di antara -2 sampai 5,5 dalam magnitudo tampak namun tidak mudah terlihat karena sudut pandangnya dengan Matahari kecil (dengan rentangan paling jauh sebesar 28,3 derajat.'),
+                child: Text(
+                    'Merkurius adalah planet terkecil di dalam tata surya dan juga yang terdekat dengan Matahari dengan kala revolusi 88 hari dan kala rotasi 59 hari. '
+                    'Kecerahan planet ini berkisar di antara -2 sampai 5,5 dalam magnitudo tampak namun tidak mudah terlihat karena sudut pandangnya dengan Matahari kecil (dengan rentangan paling jauh sebesar 28,3 derajat.'),
               ),
             ],
           )
@@ -46,9 +43,7 @@ class MyApp extends StatelessWidget {
     Card(
       semanticContainer: true,
       clipBehavior: Clip.antiAliasWithSaveLayer,
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10.0)
-      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
       margin: EdgeInsets.all(10),
       elevation: 5,
       child: Column(
@@ -67,15 +62,13 @@ class MyApp extends StatelessWidget {
                     onPressed: () {
                       print('I Like it!');
                     },
-                  )
-              ),
+                  )),
               Container(
                 padding: EdgeInsets.fromLTRB(16, 1, 16, 16),
                 child: Text(
-                    'Bumi adalah planet ketiga dari Matahari yang merupakan planet terpadat dan terbesar kelima dari '
-                        'delapan planet dalam Tata Surya. Bumi juga merupakan planet terbesar dari empat planet kebumian Tata Surya. '
-                        'Bumi terkadang disebut dengan dunia atau Planet Biru.'
-                ),
+                    'Venus adalah planet terdekat kedua dari Matahari setelah Merkurius. Planet ini mengorbit Matahari selama 224,7 hari Bumi. '
+                    'Venus tidak memiliki satelit alami dan dinamai dari dewi cinta dan kecantikan dalam mitologi Romawi. Setelah Bulan, planet ini merupakan objek alami tercerah di langit malam, '
+                    'dengan magnitudo tampak Sebesar −4,6 yang cukup cerah untuk menghasilkan bayangan.'),
               ),
             ],
           )
@@ -85,9 +78,7 @@ class MyApp extends StatelessWidget {
     Card(
       semanticContainer: true,
       clipBehavior: Clip.antiAliasWithSaveLayer,
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10.0)
-      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
       margin: EdgeInsets.all(10),
       elevation: 5,
       child: Column(
@@ -106,15 +97,13 @@ class MyApp extends StatelessWidget {
                     onPressed: () {
                       print('I Like it!');
                     },
-                  )
-              ),
+                  )),
               Container(
                 padding: EdgeInsets.fromLTRB(16, 1, 16, 16),
                 child: Text(
                     'Bumi adalah planet ketiga dari Matahari yang merupakan planet terpadat dan terbesar kelima dari '
-                        'delapan planet dalam Tata Surya. Bumi juga merupakan planet terbesar dari empat planet kebumian Tata Surya. '
-                        'Bumi terkadang disebut dengan dunia atau Planet Biru.'
-                ),
+                    'delapan planet dalam Tata Surya. Bumi juga merupakan planet terbesar dari empat planet kebumian Tata Surya. '
+                    'Bumi terkadang disebut dengan dunia atau Planet Biru.'),
               ),
             ],
           )
@@ -124,9 +113,7 @@ class MyApp extends StatelessWidget {
     Card(
       semanticContainer: true,
       clipBehavior: Clip.antiAliasWithSaveLayer,
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10.0)
-      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
       margin: EdgeInsets.all(10),
       elevation: 5,
       child: Column(
@@ -145,15 +132,13 @@ class MyApp extends StatelessWidget {
                     onPressed: () {
                       print('I Like it!');
                     },
-                  )
-              ),
+                  )),
               Container(
                 padding: EdgeInsets.fromLTRB(16, 1, 16, 16),
                 child: Text(
                     'Mars adalah planet terdekat keempat dari Matahari. Namanya diambil dari dewa perang Romawi, Mars.'
-                        'Planet ini sering dijuluki sebagai "planet merah" karena tampak dari jauh berwarna kemerah-kemerahan.'
-                        'Ini disebabkan oleh keberadaan besi(III) oksida di permukaan planet Mars.'
-                ),
+                    'Planet ini sering dijuluki sebagai "planet merah" karena tampak dari jauh berwarna kemerah-kemerahan.'
+                    'Ini disebabkan oleh keberadaan besi(III) oksida di permukaan planet Mars.'),
               ),
             ],
           )
@@ -163,9 +148,7 @@ class MyApp extends StatelessWidget {
     Card(
       semanticContainer: true,
       clipBehavior: Clip.antiAliasWithSaveLayer,
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10.0)
-      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
       margin: EdgeInsets.all(10),
       elevation: 5,
       child: Column(
@@ -177,22 +160,20 @@ class MyApp extends StatelessWidget {
           Column(
             children: <Widget>[
               ListTile(
-                  title: Text('Jupiter'),
+                  title: Text('Yupiter'),
                   subtitle: Text('Planet terbesar dalam tata surya'),
                   trailing: IconButton(
                     icon: Icon(Icons.star_border),
                     onPressed: () {
                       print('I Like it!');
                     },
-                  )
-              ),
+                  )),
               Container(
                 padding: EdgeInsets.fromLTRB(16, 1, 16, 16),
                 child: Text(
-                    'Mars adalah planet terdekat keempat dari Matahari. Namanya diambil dari dewa perang Romawi, Mars.'
-                        'Planet ini sering dijuluki sebagai "planet merah" karena tampak dari jauh berwarna kemerah-kemerahan.'
-                        'Ini disebabkan oleh keberadaan besi(III) oksida di permukaan planet Mars.'
-                ),
+                    'Yupiter adalah planet terdekat kelima dari Matahari setelah Merkurius, Venus, Bumi, dan Mars. Planet ini juga merupakan planet terbesar di Tata Surya. '
+                    'Yupiter merupakan raksasa gas dengan massa seperseribu massa Matahari dan dua setengah kali jumlah massa semua planet lain di Tata Surya. '
+                    'Planet ini dan raksasa gas lain di Tata Surya (yaitu Saturnus, Uranus, dan Neptunus) kadang-kadang disebut planet Jovian atau planet luar.'),
               ),
             ],
           )
@@ -202,9 +183,7 @@ class MyApp extends StatelessWidget {
     Card(
       semanticContainer: true,
       clipBehavior: Clip.antiAliasWithSaveLayer,
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10.0)
-      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
       margin: EdgeInsets.all(10),
       elevation: 5,
       child: Column(
@@ -223,15 +202,12 @@ class MyApp extends StatelessWidget {
                     onPressed: () {
                       print('I Like it!');
                     },
-                  )
-              ),
+                  )),
               Container(
                 padding: EdgeInsets.fromLTRB(16, 1, 16, 16),
-                child: Text(
-                    'Mars adalah planet terdekat keempat dari Matahari. Namanya diambil dari dewa perang Romawi, Mars.'
-                        'Planet ini sering dijuluki sebagai "planet merah" karena tampak dari jauh berwarna kemerah-kemerahan.'
-                        'Ini disebabkan oleh keberadaan besi(III) oksida di permukaan planet Mars.'
-                ),
+                child: Text('Saturnus adalah sebuah planet di tata surya yang dikenal juga sebagai planet bercincin, dan merupakan planet terbesar kedua di tata surya setelah Jupiter. '
+                'Jarak Saturnus sangat jauh dari Matahari, karena itulah Saturnus tampak tidak terlalu jelas dari Bumi. Saturnus berevolusi dalam waktu 29,46 tahun. ' 
+                'Setiap 378 hari, Bumi, Saturnus dan Matahari akan berada dalam satu garis lurus. Selain berevolusi, Saturnus juga berotasi dalam waktu yang sangat singkat, yaitu 10 jam 40 menit 24 detik.'),
               ),
             ],
           )
@@ -241,9 +217,7 @@ class MyApp extends StatelessWidget {
     Card(
       semanticContainer: true,
       clipBehavior: Clip.antiAliasWithSaveLayer,
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10.0)
-      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
       margin: EdgeInsets.all(10),
       elevation: 5,
       child: Column(
@@ -262,15 +236,12 @@ class MyApp extends StatelessWidget {
                     onPressed: () {
                       print('I Like it!');
                     },
-                  )
-              ),
+                  )),
               Container(
                 padding: EdgeInsets.fromLTRB(16, 1, 16, 16),
-                child: Text(
-                    'Mars adalah planet terdekat keempat dari Matahari. Namanya diambil dari dewa perang Romawi, Mars.'
-                        'Planet ini sering dijuluki sebagai "planet merah" karena tampak dari jauh berwarna kemerah-kemerahan.'
-                        'Ini disebabkan oleh keberadaan besi(III) oksida di permukaan planet Mars.'
-                ),
+                child: Text('Uranus (berasal dari nama Latin Ūranus untuk nama dewa Yunani Οὐρανός) adalah planet ketujuh dari Matahari. '
+                'Uranus merupakan planet yang memiliki jari-jari terbesar ketiga sekaligus massa terbesar keempat di Tata Surya. Uranus juga merupakan satu-satunya planet yang namanya berasal dari tokoh dalam mitologi Yunani, '
+                'dari versi Latinisasi nama dewa langit Yunani Ouranos.'),
               ),
             ],
           )
@@ -280,9 +251,7 @@ class MyApp extends StatelessWidget {
     Card(
       semanticContainer: true,
       clipBehavior: Clip.antiAliasWithSaveLayer,
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10.0)
-      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
       margin: EdgeInsets.all(10),
       elevation: 5,
       child: Column(
@@ -301,15 +270,13 @@ class MyApp extends StatelessWidget {
                     onPressed: () {
                       print('I Like it!');
                     },
-                  )
-              ),
+                  )),
               Container(
                 padding: EdgeInsets.fromLTRB(16, 1, 16, 16),
-                child: Text(
-                    'Mars adalah planet terdekat keempat dari Matahari. Namanya diambil dari dewa perang Romawi, Mars.'
-                        'Planet ini sering dijuluki sebagai "planet merah" karena tampak dari jauh berwarna kemerah-kemerahan.'
-                        'Ini disebabkan oleh keberadaan besi(III) oksida di permukaan planet Mars.'
-                ),
+                child: Text('Neptunus merupakan planet terjauh (kedelapan) jika ditinjau dari Matahari. Planet ini dinamai dari dewa lautan Romawi. '
+                'Neptunus merupakan planet terbesar keempat berdasarkan diameter (49.530 km) dan terbesar ketiga berdasarkan massa. '
+                'Massa Neptunus tercatat 17 kali lebih besar daripada Bumi, dan sedikit lebih besar daripada Uranus. '
+                'Neptunus mengorbit Matahari pada jarak 30,1 sa atau sekitar 4.450 juta km.'),
               ),
             ],
           )
